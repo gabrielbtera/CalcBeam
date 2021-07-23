0cc1ca3dd1d199dcd5aac725569cda11f5f97c26
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2021-07-23 03:13:33 -0300

    iplementação flechas e algumas interações

--- js/interacoes.js
+++ js/interacoes.js
@@ -1,5 +1,8 @@
 var teste = document.getElementById('calculoDaArmlong');
 var checar = document.getElementById('ArmLong');
+var v = document.getEle
+var telaFlechaI = document.getElementById('calculoDaFlechaImediata');
+var checkFlechaI = document.getElementById('arrowIme');
 
 var condicionalMomCalc = document.getElementById('condmomentocalc');
 var divMomCalc = document.getElementById('momentoDeCalcInput');
@@ -7,7 +10,39 @@ var divMomCalc = document.getElementById('momentoDeCalcInput');
 var condicionalMomAtuante = document.getElementById('condmomentoatuan');
 var divMomAtua = document.getElementById('momAtuante');
 
-var divMArcacao = document.getElementsByClassName("alinamentoInputs")[0];
+var divMArcacao = document.getElementsByClassName("alinamentoInputs");
+
+function limparChecksNavBar(indice){
+    const listaTlea = [teste, telaFlechaI];
+    const listaCheck = [checar, checkFlechaI]
+
+
+    for(let i =0; i<listaTlea.length; i++){
+        listaTlea[i].style.display = 'none'
+        divMArcacao[i].style.backgroundColor = "#0055b8"
+        if (i!= indice){
+            listaCheck[i].checked = false
+        }
+    }
+    
+
+}
+
+function mostrarCampo (check, tela, indice){
+    
+
+    if(check.checked){
+        limparChecksNavBar(indice)
+        tela.style.display = 'flex';
+        divMArcacao[indice].style.backgroundColor = "#004594"
+
+    }else {
+        tela.style.display = "none";
+        check.checked = false
+        divMArcacao[indice].style.backgroundColor = "#0055b8"
+    }
+}
+
 
 function mostrarValor() {
     
@@ -29,11 +64,11 @@ function mostrarMomentoCalculoeAtuante(condicional1,caixa1, condicional2, caixa2
         da função armadura longitudinal.
     */
     if(condicional1.checked){
-        caixa1.style.display = "flex"
-        condicional2.checked =false
-        caixa2.style.display = "none"
+        caixa1.style.display = "flex";
+        condicional2.checked =false;
+        caixa2.style.display = "none";
     }else {
-        caixa1.style.display = "none"
+        caixa1.style.display = "none";
     }
 }
 
@@ -51,8 +86,8 @@ function mostrarMomentoAtuante(){
 var btn = document.getElementById('calculaDimen');
 function main(){
     checar.onclick = function (){
-        mostrarValor();
-        
+        mostrarCampo(checar, teste, 0);
+
     }
     
     condicionalMomCalc.onclick = function (){
@@ -68,6 +103,9 @@ function main(){
             divMomAtua, 
             condicionalMomCalc,
             divMomCalc)
+    }
+    checkFlechaI.onclick = function (){
+        mostrarCampo(checkFlechaI, telaFlechaI, 1);
     }
 }
 main()
@@ -92,3 +130,4 @@ botao.onclick = function (){
     
 }
 
+var flechaImediataValor = document.getElementById('flechaImediataValor');
